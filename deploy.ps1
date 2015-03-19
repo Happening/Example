367889d71885f74c2ae0075d1b69d52da4b18dc7
Thijs Wiefferink <nlthijs48@gmail.com>
2015-03-19 20:34:14 +0100

    deploy.ps1: display errors and warnings
    This improved version changes the Windows PowerShell script to print the
    result that the Happening server returns when deploying a plugin fails.
    It will also print an exception when the zip file creation does not
    work.

--- deploy.ps1
+++ deploy.ps1
@@ -6,27 +6,43 @@ $Dest = [System.IO.Path]::GetTempFileName()
 
 # Read key from first argument
 if ($Args.length -eq 0) {
-    echo "Usage: .\$($MyInvocation.MyCommand.Name) <upload key>"
-    Exit
+	echo "Usage: .\$($MyInvocation.MyCommand.Name) <upload key>"
+	Exit
 }
 
 # Create a ZIP file, remove old one first
 If (Test-Path $Dest){
-    Remove-Item $Dest
+	Remove-Item $Dest
 }
 
 try {
-    [Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
-    [System.IO.Compression.ZipFile]::CreateFromDirectory($Src, $Dest, [System.IO.Compression.CompressionLevel]::Optimal, $false)
+	[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+	[System.IO.Compression.ZipFile]::CreateFromDirectory($Src, $Dest, [System.IO.Compression.CompressionLevel]::Optimal, $false)
 } catch {
-    echo "Unable to create a ZIP file. Aborting."
-    Exit
+	echo "Unable to create ZIP."
+	echo $_.Exception.GetType().FullName, $_.Exception.Message
+	Exit
 }
 
 # Upload the file
 try {
-    Invoke-RestMethod -Uri "https://happening.im/plugin/$($Args[0])" -InFile $Dest -Method POST
-} catch {
-    echo "Unable to post ZIP file. Is your key correct? Aborting."
-    Exit
+	$OldEAP = $ErrorActionPreference
+	$ErrorActionPreference = 'SilentlyContinue'
+	$result = Invoke-RestMethod -Uri "https://happening.im/plugin/$($Args[0])" -InFile $Dest -Method POST
+	$ErrorActionPreference = $OldEAP
+	# Print success result
+	echo $result
+} catch [Exception] {
+	echo ">>> Failed to deploy your plugin to Happening:"
+	# Print the exception message (includes the HTTP status code)
+	echo $_.Exception.Message
+	# Print the body of the server response (includes the error message from Happening)
+	$result = $_.Exception.Response.GetResponseStream()
+	$reader = New-Object System.IO.StreamReader($result)
+	$reader.BaseStream.Position = 0
+	$reader.DiscardBufferedData()
+	$responseBody = $reader.ReadToEnd();
+	echo $responseBody
+	Exit
 }
+
